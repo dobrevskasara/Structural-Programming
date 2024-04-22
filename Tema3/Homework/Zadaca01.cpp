ef01e844aca11239a66945affde93f5f39951c9f
dobrevskasara <149826270+dobrevskasara@users.noreply.github.com>
2024-04-22 23:40:31 +0200

    Update Zadaca01.cpp

--- Tema3/Homework/Zadaca01.cpp
+++ Tema3/Homework/Zadaca01.cpp
@@ -1,1 +1,17 @@
+//Задача 1
+//Напиши програма каде преку тастатура ќе може да се внеси цел број.
+//Програмата треба да провери дали бројот се наоѓа во интервал од 30 до 50 и
+//соодветно треба да се отпечати “Brojot __ pripaga na intervalot [30, 50]”.
 
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter number: "<<endl;
+    cin>>number;
+
+    if(number>=30 && number<=50){
+        cout<<"Number "<<number<<" belonges in interval [30, 50].";
+        }
+    return 0;
+}
