f1d3b6b651951e4fe86f00a2449932fa373077a7
dobrevskasara <149826270+dobrevskasara@users.noreply.github.com>
2024-04-26 16:38:28 +0200

    Create Zadaca01.cpp

---
+++ Tema4/Lesson/Zadaca01.cpp
@@ -0,0 +1,12 @@
+//Задача 1
+//Напиши програма каде ќе се испечатат сите броеви од 1-100.
+//Резултатот да се отпечати.
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<100;i++){
+        cout<<"Number is: "<<(i+1)<<endl;
+    }
+    return 0;
+}
