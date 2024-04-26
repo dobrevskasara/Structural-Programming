7c4dd3af4f494064bfb1c0732fd3c8c91b33af86
dobrevskasara <149826270+dobrevskasara@users.noreply.github.com>
2024-04-26 16:42:36 +0200

    Create Zadaca02.cpp

---
+++ Tema4/Lesson/Zadaca02.cpp
@@ -0,0 +1,14 @@
+//Задача 2
+//Напиши програма каде ќе се пресмета сума од сите броеви од 1-100 кои се деливи
+//со 5. Резултатот да се отпечати.
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<100;i++){
+        if((i+1)%5==0){
+            cout<<"Number "<<(i+1)<<" is divisible with 5."<<endl;
+        }
+    }
+    return 0;
+}
