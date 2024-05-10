8b8761f5f2de8758e539bebc02804685b3358c4b
dobrevskasara <149826270+dobrevskasara@users.noreply.github.com>
2024-05-11 00:16:00 +0200

    Update Zadaca01.cpp

--- Tema5/Homework/Zadaca01.cpp
+++ Tema5/Homework/Zadaca01.cpp
@@ -1,1 +1,24 @@
+//Задача 1
+//Напиши функција која е потребно да провери дали даден број е прост (бројот е
+//зададен како аргумент во функцијата). Функцијата треба да врати 0, доколку бројот
+//не е прост и 1, доколку бројот е прост
 
+#include <iostream>
+using namespace std;
+
+bool isPrime(int number){
+    for(int i=2;i<number;i++){
+        if(number%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    int number;
+    cout<<"Enter number: ";
+    cin>>number;
+    cout<<isPrime(number);
+    return 0;
+}
